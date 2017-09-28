ee7f17686ee6fba7050da65f693a4504f5408937
Oleksii Remizov <oleksii.remizov@synapse.com>
2017-09-28 20:41:31 +0300

    First commit

---
+++ RangeExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Container container = new Container();
+            foreach (var item in container)
+            {
+                Console.WriteLine("Show me");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public class Container
+    {
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator();
+        }
+    }
+
+    public class Enumerator
+    {
+        public bool MoveNext()
+        {
+            return false;
+        }
+
+        public object Current
+        {
+            get{ return null; }
+        }
+    }
+}
